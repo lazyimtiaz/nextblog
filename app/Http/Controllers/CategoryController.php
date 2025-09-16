32bf6eb51eec792eff9b03c5f9167d503b8037fc
lazyimtiaz <imtiazuddin782001@gmail.com>
2025-09-17 00:16:26 +0600

    Modified category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
 
 class CategoryController extends Controller
 {
-    //
+    public function show($slug)
+    {
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $posts = $category->posts()->with('user')->latest()->paginate(10);
+
+        return view('category.show', compact('category', 'posts'));
+    }
 }
