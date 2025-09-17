0ab87621608995d7eae5516e4bbd1d3a76060cf8
lazyimtiaz <imtiazuddin782001@gmail.com>
2025-09-17 20:35:56 +0600

    modified homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::with(['category', 'tags', 'user'])->latest()->paginate(10);
+        return view('blog.index', compact('posts'));
+    }
 }
